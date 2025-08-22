d412aca0b5d82e1ac1ab6e4c47208c178f2890ba
altar <44545494+altarluser@users.noreply.github.com>
2025-08-22 15:36:54 +0200

    Update reward_referencegame.py

--- grpo_rewards/reward_referencegame.py
+++ grpo_rewards/reward_referencegame.py
@@ -1,6 +1,20 @@
-def reward_referencegame(completion: str, prefix: list[dict[str, str]]) -> float:
+import re
+
+def reward_referencegame(completion: str) -> float:
+    """
+    Reward function for the referencegame format compliance. Only checks the instruction following attribute.
+
+    Args:
+        completion: Player's completion string
+1
+    Returns:
+        Float reward: 1.0 if completion follows format, else 0.0
+    """
     print(f"\n--------------------------------")
     print(f"completion: {completion}")
-    print(f"prefix: {prefix[0]['content']}")
     print(f"--------------------------------\n")
+
+    # Check if the completion starts correctly
+    if re.match(r"^Expression:\s+.+", completion.strip()):
+        return 1.0
     return 0.0
